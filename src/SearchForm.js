4fac8f1a134390e00ef527635281cfff1b067b33
norstein21 <habieputra3@gmail.com>
2022-12-27 18:33:04 +0700

    update search page

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import { useGlobalContext } from './context'
+
 const SearchForm = () => {
-  return <h2>Fira abis makan fogo y</h2>
+  const {query,setQuery} = useGlobalContext()
+
+  return (
+    <form className='search-form' onSubmit={(e)=>e.preventDefault()}>
+      <h2>Fira abis makan fogo y</h2>
+      <p>Cari Film Apa Aja Disini..!</p>
+      <input
+      type='text'
+      className='form-input'
+      value={query}
+      onChange={(e)=>setQuery(e.target.value)}
+      />
+    </form>
+  )
 }
 
 export default SearchForm
