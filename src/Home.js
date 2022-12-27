a6d77c561aa8f635b305d104069c1ab4ea11fdb1
norstein21 <habieputra3@gmail.com>
2022-12-27 17:47:17 +0700

    setup+init

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './SearchForm'
+import Movies from './Movies'
+
+const Home = () => {
+  return (
+    <>
+      <Form />
+      <Movies />
+    </>
+  )
+}
+
+export default Home
---
+++ src/context.js
@@ -0,0 +1,42 @@
+import React, { useState, useContext, useEffect } from 'react'
+// make sure to use https
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_KEY}`
+console.log(API_ENDPOINT)
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [isLoading,setIsLoading] = useState(true);
+  const [dataMovie,setDataMovie] = useState([]);
+  const [error,setError] = useState({show:false,message:''});
+  const [query,setQuery] = useState('one piece')
+
+  const fetchFilm = async (url) =>{
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+      if(data.Response === 'True'){
+        setDataMovie(data.Search)
+        setError({show:false,msg:''})
+      } else{
+        setError({show:true,msg:data.Error})
+      }
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchFilm(`${API_ENDPOINT}&s=${query}`)
+  },[query])
+
+  return <AppContext.Provider value={{isLoading, error,dataMovie,query,setQuery}}>{children}</AppContext.Provider>
+}
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
